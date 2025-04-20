3df28cc886e8a59c260cc64e51ffd146cee4ea50
caydebu <cayden.q.buchanan@gmail.com>
2025-04-19 17:39:18 -0700

    finished encryption and tested it out

--- encryption/main.c
+++ encryption/main.c
@@ -1,21 +1,57 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
-void encrypt(char *data, char *password){
-    	int dataLen = strlen(data);
-    	int passLen = strlen(password);
+// Global password
+const char *password = "SUPERSECRETPASSWORD";
 
-    	for (int i = 0; i < dataLen; i++) {
-        	data[i] ^= password[i % passLen]; 
-	}	
+void encrypt(char *data, const char *password) {
+        // XOR encryption
+    	for (int i = 0; i < 1024; i++) {
+        	data[i] ^= password[i % strlen(password)]; 
+    	}
+}
+
+// Role Play 🤪
+typedef struct server{
+    char data[1024];
+} server;
+
+void sendMessage(char message[], server *destination){
+
+    // Creates empty message of blanks
+    char *encryptedMessage = calloc(1024, sizeof(char));
+    memset(encryptedMessage, ' ', 1024);
+
+    // copies and encrypts messages
+    strcpy(encryptedMessage, message);
+    encrypt(encryptedMessage, password);
+
+    // copys encrypted message into server
+    memcpy(destination->data, encryptedMessage, 1024);
 
 }
 
-int main()
-{
-	char string[] = "hello I am testing the decryption technique ive thought of my self";
-	encrypt(string, "ThisIsMyTestPassword");
-	encrypt(string, "ThisIsMyTestPassword");
-	printf(string);
+char* recieveMessage(server *destination){
+
+    // gets encrypted message from server
+    char *decryptedMessage = malloc(1024);
+    memcpy(decryptedMessage, destination->data, 1024);
+
+    // decryptes and return message
+    encrypt(decryptedMessage, password);
+    return decryptedMessage;
 }
+
+int main() {
+    	char message[] = "hello I am testing the decryption technique ive totally thought of my self";
+        server Server;
+
+        sendMessage(message, &Server);
+        char *recievedMessage = recieveMessage(&Server);
+    	printf("%.*s\n", 1024, recievedMessage); // No clue what how the hell this works skips null term
+
+    	return 0;
+    
+}
+
