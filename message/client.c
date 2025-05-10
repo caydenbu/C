81d059b4346d0519bc5b1606944e96bcc36189b2
caydebu <cayden.q.buchanan@gmail.com>
2025-05-09 22:14:20 -0700

    server start

---
+++ message/client.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define SERVER_IP "127.0.0.1"
+#define PORT 12345
+
+int main(){
+    printf("test");
+    return 0;
+}
