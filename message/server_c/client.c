40083693eef700831f39a800fdfc1568d149c202
caydenbu <cayden.q.buchanan@gmail.com>
2025-06-02 22:02:00 -0700

    End of project! Added multithreading and fixed UI

--- message/server_c/client.c
+++ message/server_c/client.c
@@ -1,5 +1,6 @@
 #include "raylib.h"
 #include <arpa/inet.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,17 +12,39 @@
 #define SERVER_IP "127.0.0.1"
 #define PORT 8080
 
-int main(int argc, char const *argv[]) {
+#define MAX_MESSAGES 10
 
-    // --------- Full socket connection to server ---------- //
-    int status, valread, client_fd;
+// Shared state
+char *messages[MAX_MESSAGES] = {0};
+int messageCount = 0;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+int client_fd;
+
+// Receiver thread
+void *receiver_thread(void *arg) {
+    char buffer[1024];
+    while (1) {
+        ssize_t valread = read(client_fd, buffer, sizeof(buffer) - 1);
+        if (valread <= 0)
+            break;
+        buffer[valread] = '\0';
+
+        pthread_mutex_lock(&lock);
+        if (messageCount < MAX_MESSAGES) {
+            messages[messageCount++] = strdup(buffer);
+        }
+        pthread_mutex_unlock(&lock);
+    }
+    return NULL;
+}
+
+int main() {
     struct sockaddr_in serv_addr;
-
-    char buffer[1024] = {0};
 
     if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("socket failed");
-        //       return -1;
+        return 1;
     }
 
     serv_addr.sin_family = AF_INET;
@@ -29,85 +52,77 @@ int main(int argc, char const *argv[]) {
 
     if (inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr) <= 0) {
         printf("invalid address\n");
-        //     return -1;
+        return 1;
     }
 
-    if ((status = connect(client_fd, (struct sockaddr *)&serv_addr,
-                          sizeof(serv_addr))) < 0) {
+    if (connect(client_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) <
+        0) {
         printf("Connection Failed\n");
-        //   return -1;
+        return 1;
     }
 
-    // --------- Raylib window ---------- //
+    pthread_t recv_thread;
+    pthread_create(&recv_thread, NULL, receiver_thread, NULL);
+
+    // Raylib
     InitWindow(800, 600, "Messaging Project");
-
     SetTargetFPS(60);
-    int tick = 0;
 
-    // --------- Game and network loop ---------- //
+    int tick = 0;
     char *text = malloc(1024);
     text[0] = '\0';
-
-    char *CurrentMessage = malloc(1024);
-    CurrentMessage[0] = '\0';
-
-    int messages = 0; //  Change this to a dynamic list later
 
     while (!WindowShouldClose()) {
         tick++;
 
+        // Input handling
+        char key = GetCharPressed();
+        while (key > 0) {
+            int len = strlen(text);
+            if (len < 1022) {
+                text[len] = key;
+                text[len + 1] = '\0';
+            }
+            key = GetCharPressed();
+        }
+
+        if (IsKeyPressed(KEY_BACKSPACE)) {
+            int len = strlen(text);
+            if (len > 0)
+                text[len - 1] = '\0';
+        }
+
         if (IsKeyPressed(KEY_ENTER)) {
-            // Sends Message
             send(client_fd, text, strlen(text), 0);
-            valread = read(client_fd, buffer, sizeof(buffer) - 1);
-
-            // Recieves back message
-            messages++;
-            buffer[valread] = '\0';
-            CurrentMessage = buffer;
-            printf("%s\n", buffer);
-
-            // Resets Clients text
             free(text);
             text = malloc(1024);
             text[0] = '\0';
         }
 
-        // UI Init
-        Color background;
-        background.r = 10;
-        background.g = 10;
-        background.b = 10;
-
-        // Get Input
-        char key = GetCharPressed();
-
-        if (key > 0) {
-            text[strlen(text)] = key;
-            text[strlen(text) + 1] = '\0';
-        }
-
-        // BackSpace
-        if (IsKeyPressed(KEY_BACKSPACE)) {
-            int len = strlen(text);
-            if (len > 0) {
-                text[len - 1] = '\0';
-            }
-        }
-
-        // Draw
+        // Drawing
         BeginDrawing();
-        ClearBackground(background);
-
+        ClearBackground((Color){10, 10, 10, 255});
         DrawInputBox(text, tick);
-        DrawText(CurrentMessage, 0, 0, 32, WHITE);
+
+        pthread_mutex_lock(&lock);
+        for (int i = 0; i < messageCount; i++) {
+            DrawText(messages[i], 10, 40 + i * 32, 28, WHITE);
+        }
+        pthread_mutex_unlock(&lock);
 
         EndDrawing();
     }
 
     // Cleanup
+    pthread_cancel(recv_thread);
+    pthread_join(recv_thread, NULL);
+
+    for (int i = 0; i < messageCount; i++) {
+        free(messages[i]);
+    }
+
+    free(text);
     CloseWindow();
     close(client_fd);
-
     return 0;
 }
--- message/server_c/server.c
+++ message/server_c/server.c
@@ -1,4 +1,5 @@
 #include <netinet/in.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,70 +7,84 @@
 #include <unistd.h>
 
 #define PORT 8080
+#define MAX_CLIENTS 10
+
+int clients[MAX_CLIENTS];
+int client_count = 0;
+pthread_mutex_t client_lock = PTHREAD_MUTEX_INITIALIZER;
+
+void broadcast(const char *msg, int sender_fd) {
+    pthread_mutex_lock(&client_lock);
+    for (int i = 0; i < client_count; i++) {
+        send(clients[i], msg, strlen(msg), 0);
+    }
+    pthread_mutex_unlock(&client_lock);
+}
+
+void *handle_client(void *arg) {
+    int client_fd = *(int *)arg;
+    free(arg);
+    char buffer[1024];
+
+    while (1) {
+        ssize_t len = read(client_fd, buffer, sizeof(buffer) - 1);
+        if (len <= 0)
+            break;
+
+        buffer[len] = '\0';
+        broadcast(buffer, client_fd); // send to everyone else
+    }
+
+    pthread_mutex_lock(&client_lock);
+    for (int i = 0; i < client_count; i++) {
+        if (clients[i] == client_fd) {
+            clients[i] = clients[--client_count];
+            break;
+        }
+    }
+    pthread_mutex_unlock(&client_lock);
+
+    close(client_fd);
+    return NULL;
+}
+
 int main() {
-
     int server_fd, new_socket;
-    ssize_t valread;
     struct sockaddr_in address;
-    int opt = 1;
     socklen_t addrlen = sizeof(address);
-    char buffer[1024] = {0};
 
-    // --------- Full socket connection to server ---------- //
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    // Create the socket
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Attach socket to port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // Bind socket
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    // Listen
-    if (listen(server_fd, 3) < 0) {
-        perror("listen fail");
-        exit(EXIT_FAILURE);
-    }
-    // Accept & new socket
-    if ((new_socket =
-             accept(server_fd, (struct sockaddr *)&address, &addrlen)) < 0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    listen(server_fd, MAX_CLIENTS);
 
-    // --------- Send and Read message ---------- //
     while (1) {
+        new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen);
+        if (new_socket < 0)
+            continue;
 
-        valread = read(new_socket, buffer, 1024 - 1);
+        pthread_mutex_lock(&client_lock);
+        if (client_count < MAX_CLIENTS) {
+            clients[client_count++] = new_socket;
 
-        if (valread <= 0)
-            break; // client left
-
-        buffer[valread] = '\0';
-
-        // send(new_socket, message, strlen(message), 0);
-        send(new_socket, buffer, strlen(buffer), 0);
+            int *pclient = malloc(sizeof(int));
+            *pclient = new_socket;
+            pthread_t tid;
+            pthread_create(&tid, NULL, handle_client, pclient);
+            pthread_detach(tid);
+        } else {
+            close(new_socket); // too many clients
+        }
+        pthread_mutex_unlock(&client_lock);
     }
 
-    close(new_socket);
     close(server_fd);
-
     return 0;
 }
